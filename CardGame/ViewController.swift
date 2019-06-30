f918770a3020a16c89915eafed184537b31dad7e
RongX <sharoneaecs@gmail.com>
2019-06-30 11:05:46 -0700

    course commited

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var leftImageView: UIImageView!
+    
+    @IBOutlet weak var rightImageView: UIImageView!
+    
+    @IBOutlet weak var leftScoreView: UILabel!
+    
+    @IBOutlet weak var rightScoreView: UILabel!
+    
+    var leftScore = 0;
+    var rightScore = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func dealTapped(_ sender: Any) {
+        print("deal tapped.")
+        let leftNum = Int.random(in: 2...14)
+        
+        let rightNum = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNum)")
+        rightImageView.image = UIImage(named: "card\(rightNum)")
+        
+        if leftNum > rightNum{
+            leftScore += 1
+            leftScoreView.text = String(leftScore)
+        }
+        else if leftNum < rightNum{
+            rightScore += 1
+            rightScoreView.text = "\(rightScore)"
+        }
+        else{
+            
+        }
+        
+    }
+    
 }
 
