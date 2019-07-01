a592f65fd2549e09ce12566552dffcfac8a2ae77
RongX <sharoneaecs@gmail.com>
2019-06-30 18:55:45 -0700

    Dry fix.

--- CardGame/ViewController.swift
+++ CardGame/ViewController.swift
@@ -22,19 +22,18 @@ class ViewController: UIViewController {
     var leftScore = 0;
     var rightScore = 0;
     
+    var leftNum = 2
+    var rightNum = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        ramdonCard()
     }
 
     @IBAction func dealTapped(_ sender: Any) {
-        print("deal tapped.")
-        let leftNum = Int.random(in: 2...14)
-        
-        let rightNum = Int.random(in: 2...14)
-        
-        leftImageView.image = UIImage(named: "card\(leftNum)")
-        rightImageView.image = UIImage(named: "card\(rightNum)")
+        //print("deal tapped.")
+        ramdonCard()
         
         if leftNum > rightNum{
             leftScore += 1
@@ -45,8 +44,19 @@ class ViewController: UIViewController {
             rightScoreView.text = "\(rightScore)"
         }
         else{
-            
+            // tie, no action
         }
+        
+    }
+    
+    private func ramdonCard(){
+        leftNum = Int.random(in: 2...14)
+        
+        rightNum = Int.random(in: 2...14)
+        
+        leftImageView.image = UIImage(named: "card\(leftNum)")
+        rightImageView.image = UIImage(named: "card\(rightNum)")
+        
         
     }
     
